ceed51ccadf158981bbdd0bf2832c9f8c88bcd9c
Protryon <max.bruce12@gmail.com>
2021-01-05 10:02:35 -0800

    add malformed date to lookup table

--- src/lib/error-lookup.ts
+++ src/lib/error-lookup.ts
@@ -1,7 +1,8 @@
 const errorLookupTable = {
     EMAIL_NOT_FOUND: 'We didn\'t recognize your email. If your email is correct, request an invite to VallurX from your manager.',
     INVALID_PASSWORD: 'Your password is incorrect.',
-    INVALID_2FA_CODE: 'Your Two-Factor Authentication code is incorrect.'
+    INVALID_2FA_CODE: 'Your Two-Factor Authentication code is incorrect.',
+    MALFORMED_DATE: 'Entered date was not of correct format: Month/Day/Year',
 };
 
 export type ErrorTypes = keyof typeof errorLookupTable;
